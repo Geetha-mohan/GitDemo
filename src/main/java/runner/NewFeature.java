f27b0321d4ef604992cba18e85b18279eb7a2367
Geetha <geethamohanqa@gmail.com>
2025-01-12 09:55:46 -0500

    changed webhook url

--- src/main/java/runner/NewFeature.java
+++ src/main/java/runner/NewFeature.java
@@ -11,7 +11,7 @@ public class NewFeature extends ProjectSpecMethods {
 
 	@Test()
 	public void runNewFeature() {
-		System.out.println("new feature deleted");
+		
 		System.out.println("new feature added");
 	}
 
